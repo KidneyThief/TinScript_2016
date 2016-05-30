be94429497394d02b5d7b1f5a1bbe586f4f516f8
Tim Andersen <kidneythief@shaw.ca>
2016-05-29 21:48:26 -0700

    *  Worms.ts   testing network

--- TinQtDemo/worms.ts
+++ TinQtDemo/worms.ts
@@ -67,6 +67,10 @@ void Player::SetDirection(int direction)
 
     // -- set the direction
     self.m_nextDirection = direction;
+
+    // -- if we're the challenger, notify the host
+    if (!gCurrentGame.m_isHost)
+        SocketCommand("NotifyPlayerMove", direction);
 }
 
 void Player::OnUpdate(float deltaTime)
@@ -130,27 +134,47 @@ void Player::OnUpdate(float deltaTime)
             // -- see if we ate the apple
             if (gCurrentGame.FoundApplePosition(self.position))
             {
-                self.length += 1;
+                self.length += 3;
             }
-
-            // -- and the cancel the tail draw request
-            int tail_index = (self.m_currentDrawRequest + 3072 - self.length) % 3072;
-            CancelDrawRequests(tail_index);
-
-            // -- set the tail position unoccupied
-            gCurrentGame.SetPositionOccupied(self.m_tail[tail_index], false);
-
-            // -- update the head position
-            self.m_currentDrawRequest = (self.m_currentDrawRequest + 1) % 3072;
-
-            // -- set the head position occupied
-            gCurrentGame.SetPositionOccupied(self.position, true);
-            self.m_tail[self.m_currentDrawRequest] = self.position;
         }
+    }
+
+    // -- update the player's position
+    self.NotifyPosition();
+}
+
+void Player::NotifyPosition()
+{
+    bool is_local_player = (self == gCurrentGame.m_localPlayer);
+
+    int draw_request_offset = 0;
+    if (!is_local_player)
+        draw_request_offset = 4000;
+
+    int player_color =gCOLOR_BLUE;
+    if (!is_local_player)
+        player_color = gCOLOR_GREEN;
+
+    // -- if the position is occupied, game over
+    if (!gCurrentGame.m_isHost || self.m_direction != g_directionNone)
+    {
+        // -- and the cancel the tail draw request
+        int tail_index = (self.m_currentDrawRequest + 3072 - self.length) % 3072;
+        CancelDrawRequests(tail_index + draw_request_offset);
+
+        // -- set the tail position unoccupied
+        gCurrentGame.SetPositionOccupied(self.m_tail[tail_index], false);
+
+        // -- update the head position
+        self.m_currentDrawRequest = (self.m_currentDrawRequest + 1) % 3072;
+
+        // -- set the head position occupied
+        gCurrentGame.SetPositionOccupied(self.position, true);
+        self.m_tail[self.m_currentDrawRequest] = self.position;
     }
     else
     {
-        CancelDrawRequests(0);
+        CancelDrawRequests(0 + draw_request_offset);
         self.m_currentDrawRequest = 0;
         self.m_tail[0] = self.position;
     }
@@ -160,16 +184,25 @@ void Player::OnUpdate(float deltaTime)
     draw_position:y *= g_playerSize;
     
     // -- draw the 4 lines creating the "triangle-ish" ship
-    DrawRect(self.m_currentDrawRequest, draw_position, g_playerSize, g_playerSize, gCOLOR_BLUE);
+    DrawRect(self.m_currentDrawRequest + draw_request_offset, draw_position, g_playerSize, g_playerSize, player_color);
+
+    // -- notify the client
+    SocketCommand("NotifyPlayerUpdate", is_local_player, self.position, self.length);
 }
 
 void Player::OnCollision()
 {
     // -- game over, at the center of the screen
-    DrawText(self, '320 240 0', "G A M E   O V E R", gCOLOR_RED);
+    bool is_local_player = (self == gCurrentGame.m_localPlayer);
+    if (is_local_player)
+        DrawText(self, '320 240 0', "Y O U   L O S E", gCOLOR_RED);
+    else
+        DrawText(self, '320 240 0', "Y O U   W I N", gCOLOR_RED);
+
+    // -- notify the client
+    SocketCommand("NotifyGameOver", is_local_player);
     
     SimPause();
-    Print("Type StartWorms(); to continue...");
 }
 
 // ====================================================================================================================
@@ -192,6 +225,13 @@ void WormsGame::OnCreate() : DefaultGame
     bool self.m_hasApple = false;
     vector3f self.m_apple;
     int self.m_appleTime = GetSimTime() + 4000;
+
+    // -- issue the countdown
+    int self.m_updateTime = GetSimTime();
+    int self.m_gameCountdown = 5000;
+    bool self.m_gameStarted = false;
+    bool self.m_isHost = false;
+    int self.m_clientUpdateTime = 0;
 }
 
 void WormsGame::OnUpdate()
@@ -199,22 +239,56 @@ void WormsGame::OnUpdate()
     // -- initialize the hierarchy
     DefaultGame::OnUpdate();
 
-    // -- update the input
-    self.UpdateKeys(self.SimTime);
-
-    int cur_time = GetSimTime();
-    int elapsed = cur_time - self.m_lastUpdateTime;
-    if (elapsed >= self.m_updatePeriod)
+    // -- if we haven't started yet, we're done
+    if (!self.m_gameStarted)
     {
-        self.m_lastUpdateTime = cur_time;
+        self.m_updateTime = GetSimTime();
+        return;
+    }
 
+    // -- perform the countdown
+    if (self.m_gameCountdown > 0)
+    {
+        int deltaTime = GetSimTime() - self.m_updateTime;
+        self.m_gameCountdown -= deltaTime;
+        int seconds = (self.m_gameCountdown / 1000) + 1;
+
+        // -- game over, at the center of the screen
+        CancelDrawRequests(8000);
+        DrawText(8000, '320 240 0', seconds, gCOLOR_RED);
+
+        // -- start both players moving
         object player = self.m_playerGroup.First();
         while (IsObject(player))
         {
-            player.OnUpdate(0.0f);
+            player.m_direction = g_directionUp;
             player = self.m_playerGroup.Next();
         }
     }
+
+    // -- update the input
+    self.UpdateKeys(self.SimTime);
+
+    // -- only the host updates the players
+    if (gCurrentGame.m_isHost)
+    {
+        int cur_time = GetSimTime();
+        int elapsed = cur_time - self.m_lastUpdateTime;
+        if (elapsed >= self.m_updatePeriod)
+        {
+            self.m_lastUpdateTime = cur_time;
+
+            object player = self.m_playerGroup.First();
+            while (IsObject(player))
+            {
+                player.OnUpdate(0.0f);
+                player = self.m_playerGroup.Next();
+            }
+        }
+    }
+
+    if (self.m_gameCountdown > 0)
+        return;
 
     // -- see if it's time to create an apple
     if (self.SimTime > self.m_appleTime && !self.m_hasApple)
@@ -223,15 +297,15 @@ void WormsGame::OnUpdate()
     }
 
     // -- draw the apple
+    CancelDrawRequests(8000);
     if (self.m_hasApple)
     {
-        CancelDrawRequests(4000);
         vector3f draw_position = self.m_apple;
         draw_position:x *= g_playerSize;
         draw_position:y *= g_playerSize;
         
         // -- draw the 4 lines creating the "triangle-ish" ship
-        DrawRect(4000, draw_position, g_playerSize, g_playerSize, gCOLOR_RED);
+        DrawRect(8000, draw_position, g_playerSize, g_playerSize, gCOLOR_RED);
     }
 }
 
@@ -262,8 +336,12 @@ bool WormsGame::FoundApplePosition(vector3f position)
     {
         self.m_hasApple = false;
         self.m_updatePeriod -= 5;
-        if (self.m_updatePeriod < 1)
-            self.m_updatePeriod = 1;
+        if (self.m_updatePeriod < 10)
+            self.m_updatePeriod = 10;
+
+        // -- update the spawn time
+        self.m_appleTime = self.SimTime + 4000;
+
         return (true);
     }
 
@@ -302,9 +380,6 @@ object WormsGame::CreatePlayer(string name, bool is_local, vector3f position)
     object player = create Player(name);
     player.Initialize(position);
 
-    // -- add the player to the game objects set, so it receives OnUpdate() calls
-    self.game_objects.AddObject(player);
-
     // -- specific organization for this game
     self.m_playerGroup.AddObject(player);
     if (is_local)
@@ -315,6 +390,9 @@ object WormsGame::CreatePlayer(string name, bool is_local, vector3f position)
 
 void WormsGame::UpdateKeys(int update_time)
 {
+    if (self.m_gameCountdown > 0)
+        return;
+
     object local_player = self.m_localPlayer;
     if (!IsObject(local_player))
         return;
@@ -336,14 +414,73 @@ void WormsGame::UpdateKeys(int update_time)
         local_player.SetDirection(g_directionDown);
 }
 
-void StartWorms()
+void StartWorms(string player_name)
 {
     CancelDrawRequests(-1);
     ResetGame();
     gCurrentGame = create WormsGame("CurrentGame");
+    gCurrentGame.m_isHost = true;
 
     // -- create the player
-    gCurrentGame.CreatePlayer("Foo", true, "32 24 0");
+    gCurrentGame.CreatePlayer(player_name, true, "22 24 0");
+
+    // -- this will be the host
+    SocketListen();
+}
+
+void ChallengeWorms(string player_name, string ip_address)
+{
+    CancelDrawRequests(-1);
+    ResetGame();
+    gCurrentGame = create WormsGame("CurrentGame");
+    gCurrentGame.m_isHost = false;
+
+    // -- create the player
+    gCurrentGame.CreatePlayer(player_name, true, "42 24 0");
+
+    // -- the challenger is player two
+    SocketConnect(ip_address);
+    SocketCommand("NotifyChallenge", player_name);
+}
+
+void NotifyChallenge(string challenger_name)
+{
+    // -- create the player
+    gCurrentGame.CreatePlayer(challenger_name, false, "42 24 0");
+    SocketSend("AcceptChallenge", gCurrentGame.m_localPlayer.GetObjectName());
+
+    // -- set the bool to start the game
+    self.m_gameStarted = true;
+}
+
+void AcceptChallenge(string challenger_name)
+{
+    // -- create the player
+    gCurrentGame.CreatePlayer(challenger_name, false, "24 24 0");
+
+    // -- set the bool to notify that the game has started
+    self.m_gameStarted = true;
+}
+
+void NotifyPlayerMove(int direction)
+{
+    object challenger = gCurrentGame.m_playerGroup.GetObjectByIndex(1);
+    if (IsObject(challenger))
+    {
+        challenger.SetDirection(direction);
+    }
+}
+
+void NotifyPlayerUpdate(bool is_local_player, vector3f position, int length)
+{
+    // -- find the player
+    int player_index = 0;
+    if (!is_local_player)
+        player_index = 1;
+    object player = gCurrentGame.m_playerGroup.GetObjectByIndex(player_index);
+    player.position = position;
+    player.length = length;
+    player.NotifyUpdate();
 }
 
 // ====================================================================================================================
