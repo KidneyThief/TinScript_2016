8b3fc3fd0dccbfb511b70cd84afa229dc107a8db
kidneythief <kidneythief9@gmail.com>
2021-09-24 17:43:39 -0700

    Fixed demotools (ToolPalette) to use SocketExec()

--- TinQtDemo/demotools.ts
+++ TinQtDemo/demotools.ts
@@ -3,30 +3,30 @@
 // Test script to demonstrate populating the debugger tool palette elements to support the FLTK demo.
 // --------------------------------------------------------------------------------------------------------------------
 
-SocketCommand("ToolPaletteClear", "Asteroids");
-SocketCommand("ToolPaletteAddMessage", "Asteroids", "Demo Buttons");
-SocketCommand("ToolPaletteAddButton", "Asteroids", "Tools", "Execute demotools.ts", "Tools", "Exec('demotools.ts');");
-SocketCommand("ToolPaletteAddButton", "Asteroids", "Exec", "Execute asteroids.ts", "Exec Asteroids", "Exec('asteroids.ts');");
-SocketCommand("ToolPaletteAddButton", "Asteroids", "Start", "Start the Asteroids game", "Start", "StartAsteroids();");
-SocketCommand("ToolPaletteAddButton", "Asteroids", "Pause", "Pause the simulation", "Pause", "SimPause();");
-SocketCommand("ToolPaletteAddButton", "Asteroids", "Unpause", "Unpause the simulation", "Unpause", "SimUnpause();");
+SocketExec(hash("ToolPaletteClear"), "Asteroids");
+SocketExec(hash("ToolPaletteAddMessage"), "Asteroids", "Demo Buttons");
+SocketExec(hash("ToolPaletteAddButton"), "Asteroids", "Tools", "Execute demotools.ts", "Tools", "Exec('demotools.ts');");
+SocketExec(hash("ToolPaletteAddButton"), "Asteroids", "Exec", "Execute asteroids.ts", "Exec Asteroids", "Exec('asteroids.ts');");
+SocketExec(hash("ToolPaletteAddButton"), "Asteroids", "Start", "Start the Asteroids game", "Start", "StartAsteroids();");
+SocketExec(hash("ToolPaletteAddButton"), "Asteroids", "Pause", "Pause the simulation", "Pause", "SimPause();");
+SocketExec(hash("ToolPaletteAddButton"), "Asteroids", "Unpause", "Unpause the simulation", "Unpause", "SimUnpause();");
 
 void TogglePaused()
 {
     if (SimIsPaused())
     {
         SimUnpause();
-        SocketCommand("ToolPaletteSetEntryValue", "Asteroids::Toggle", "Pause");
-        SocketCommand("ToolPaletteSetEntryDescription", "Asteroids::Toggle", "The simulation is running.");
+        SocketExec(hash("ToolPaletteSetEntryValue"), "Asteroids::Toggle", "Pause");
+        SocketExec(hash("ToolPaletteSetEntryDescription"), "Asteroids::Toggle", "The simulation is running.");
     }
     else
     {
         SimPause();
-        SocketCommand("ToolPaletteSetEntryValue", "Asteroids::Toggle", "Unpause");
-        SocketCommand("ToolPaletteSetEntryDescription", "Asteroids::Toggle", "The simulation is paused.");
+        SocketExec(hash("ToolPaletteSetEntryValue"), "Asteroids::Toggle", "Unpause");
+        SocketExec(hash("ToolPaletteSetEntryDescription"), "Asteroids::Toggle", "The simulation is paused.");
     }
 }
-SocketCommand("ToolPaletteAddButton", "Asteroids", "Toggle", "Toggle the paused state of the simulation.", "Pause", "TogglePaused();");
+SocketExec(hash("ToolPaletteAddButton"), "Asteroids", "Toggle", "Toggle the paused state of the simulation.", "Pause", "TogglePaused();");
 
 void SetTimeScale(int time_ticks)
 {
@@ -34,6 +34,6 @@ void SetTimeScale(int time_ticks)
     new_time_scale /= 100.0f;
     SimSetTimeScale(new_time_scale);
     string new_description = StringCat("[", time_ticks, "%%]: Set the simulation time scale.");
-    SocketCommand("ToolPaletteSetEntryDescription", "Asteroids::TimeScale", new_description);
+    SocketExec(hash("ToolPaletteSetEntryDescription"), "Asteroids::TimeScale", new_description);
 }
-SocketCommand("ToolPaletteAddSlider", "Asteroids", "TimeScale", "[100%%]: Set the simulation time scale", 1, 400, 100, "SetTimeScale");
+SocketExec(hash("ToolPaletteAddSlider"), "Asteroids", "TimeScale", "[100%%]: Set the simulation time scale", 1, 400, 100, "SetTimeScale");
